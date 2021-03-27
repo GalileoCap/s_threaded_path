f578fbc355dc8425085ac8003b74f5fee2496883
GalileoCap <galileocapp@gmail.com>
2021-03-26 21:48:28 -0300

    OK: Testing threads

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
+#include <thread>
 #include <iostream>
 
+#include <string>
+#include <time.h>
+#include <unistd.h>
+
+using namespace std;
+
+time_t start;
+
+void test(const string& s) {
+    sleep(3);
+    time_t end = time(0) - start;
+    cout << s << " IN " << end << endl;
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    start = time(0);
+
+    thread t = thread(&test, "DONE");
+
+    cout << "WAITING..." << endl;
+
+    t.join();
+
+    cout << "FINISHED" << endl;
+
     return 0;
 }
